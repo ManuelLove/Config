bfca1cda2e19729b586e13bc0ea4e78418d5b3ca
ManuelLove <104648635+ManuelLove@users.noreply.github.com>
2025-03-25 16:54:35 -0500

    Add files via upload

--- case.js
+++ case.js
@@ -7835,7 +7835,7 @@ if (isUserRegistered(m.sender)) {
 							url: getRandomThumb2(), // Pastikan file ini tersedia
             gifPlayback: true
         },
-        caption: `${shomenu}`, // Teks menu
+        caption: `${shonhemenu}`, // Teks menu
         contextInfo: {
         mentionedJid: [m.sender],
             forwardingScore: 999,
