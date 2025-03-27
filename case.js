841ce72e3bc2602816dca8a935e25a34535b86e4
ManuelLove <104648635+ManuelLove@users.noreply.github.com>
2025-03-27 01:02:08 -0500

    Add files via upload

--- case.js
+++ case.js
@@ -7845,7 +7845,7 @@ if (isUserRegistered(m.sender)) {
 							{
 								title: namabot,
 								body: descown,
-								url: 'https://i.ibb.co/LDBMFzgv/file.png',
+								thumbnail: getRandomThumb2(),
 								mediaType: 1,
 								renderLargerThumbnail: true,
 								previewType: 0,
