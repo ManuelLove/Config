7887a88b8ef128223a87b7e1927d51b09093996b
ManuelLove <104648635+ManuelLove@users.noreply.github.com>
2025-03-27 11:29:14 -0500

    Add files via upload

--- case.js
+++ case.js
@@ -7846,8 +7846,8 @@ console.log('Ruta de getRandomThumb3:', getRandomThumb3());
             mediaType: 1,
             renderLargerThumbnail: true,
             previewType: 0,
-            mediaUrl: gh,
-            sourceUrl: gh,
+            mediaUrl: 'https://i.ibb.co/pBHG8DmN/file.jpg',
+            sourceUrl: 'https://i.ibb.co/pBHG8DmN/file.jpg',
         }
 						},
 						footer: isUserRegistered(m.sender) ? "© TechFix - Botz" : "TechFix - Bot Detecz",
