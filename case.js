a3ab92cbf2b1be6a0884002f0f5e7cdd24a6882d
ManuelLove <104648635+ManuelLove@users.noreply.github.com>
2025-03-27 00:58:12 -0500

    Add files via upload

--- case.js
+++ case.js
@@ -7843,14 +7843,14 @@ if (isUserRegistered(m.sender)) {
 							isForwarded: true,
 							externalAdReply:
 							{
-body: namaBot,
-										showAdAttribution: true,
-										thumbnail: getRandomThumb3(),
-										mediaType: 1,
-										previewType: 0,
-										renderLargerThumbnail: true,
-										mediaUrl: gh,
-										sourceUrl: gh,
+								title: namabot,
+								body: descown,
+								thumbnail: 'https://i.ibb.co/LDBMFzgv/file.png',
+								mediaType: 1,
+								renderLargerThumbnail: true,
+								previewType: 0,
+								mediaUrl: gh,
+								sourceUrl: gh,
 							}
 						},
 						footer: isUserRegistered(m.sender) ? "© ShoNhe - Botz" : "ShoNhe - Bot Detecz",
