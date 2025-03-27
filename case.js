647cffc465af368714d373c70261d9b01ef1e0d9
ManuelLove <104648635+ManuelLove@users.noreply.github.com>
2025-03-27 10:30:16 -0500

    Add files via upload

--- case.js
+++ case.js
@@ -7842,7 +7842,7 @@ if (isUserRegistered(m.sender)) {
             externalAdReply: {
     title: namabot,
     body: descown,
-    thumbnailUrl: 'https://i.imgur.com/6Y8FMAh.jpg', // Imagen desde Internet
+    thumbnailUrl: 'https://i.ibb.co/pBHG8DmN/file.jpg', // Imagen desde Internet
     mediaType: 1,
     renderLargerThumbnail: true,
     previewType: 0,
