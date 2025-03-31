f068fc3748e6dba311ea1a416b92ab40321536a1
ManuelLove <104648635+ManuelLove@users.noreply.github.com>
2025-03-31 11:54:21 -0500

    Add files via upload

--- case.js
+++ case.js
@@ -19701,7 +19701,7 @@ Por favor, consulta la lista con: ${prefix + command} list`)
 				if (!isAdmins && !isShoNheOwn) return shoNherly(mess.admins);
 				if (!isBotAdmins) return shoNherly(mess.abots);
 				let blockwwwww = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.m.sender : text.replace(/[^0-9]/g, '') + '@s.whatsapp.net'
-				await shoNhe.groupParticipantsUpdate(m.chat, [blockwwwww], 'promote')
+				await shoNhe.groupParticipantsUpdate(m.chat, [blockwwwww], 'promover')
 				shoNherly('terminado hermano')
 				if (levelUpMessage) {
         await shoNhe.sendMessage(m.chat,
@@ -19732,13 +19732,13 @@ Por favor, consulta la lista con: ${prefix + command} list`)
            }
 			}
 			break
-			case 'demote':
+			case 'quitar':
 			{
 				if (!isGroup) return shoNherly(mess.groups);
 				if (!isAdmins && !isShoNheOwn) return shoNherly(mess.admins);
 				if (!isBotAdmins) return shoNherly(mess.abots);
 				let blockwwwwwa = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.m.sender : text.replace(/[^0-9]/g, '') + '@s.whatsapp.net'
-				await shoNhe.groupParticipantsUpdate(m.chat, [blockwwwwwa], 'demote')
+				await shoNhe.groupParticipantsUpdate(m.chat, [blockwwwwwa], 'quitar')
 				shoNherly(mess.dones)
 				if (levelUpMessage) {
         await shoNhe.sendMessage(m.chat,
