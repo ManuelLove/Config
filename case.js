8ac81ff11adc3f6bc2eace31a71c3f31b0af2d0e
ManuelLove <104648635+ManuelLove@users.noreply.github.com>
2025-04-07 18:06:16 -0500

    Add files via upload

--- case.js
+++ case.js
@@ -29175,29 +29175,21 @@ Y su historia aún no ha terminado. Operando en la clandestinidad, siguen desarr
 						await shoNherly(require('util').format(teks))
 					}
 				}
-try {
-	if (budy.startsWith('=>') || budy.startsWith('>') || budy.startsWith('>>')) {
-		if (!isShoNheOwn) return
-		try {
-			let kode = budy.trim().split(" ")[0]
-			let teks = await eval(`(async () => { ${kode == ">>" ? "return" : ""} ${q}})()`)
-			await shoNherly(require('util').format(teks))
-		} catch (e) {
-			await shoNherly(require('util').format(e))
+				if (budy.startsWith('$'))
+				{
+					if (!isShoNheOwn) return
+					exec(budy.slice(2), (err, stdout) =>
+					{
+						if (err) return shoNherly(`${err}`)
+						if (stdout) return shoNherly(stdout)
+					})
+				}
 		}
 	}
-
-	if (budy.startsWith('$')) {
-		if (!isShoNheOwn) return
-		exec(budy.slice(2), (err, stdout) => {
-			if (err) return shoNherly(`${err}`)
-			if (stdout) return shoNherly(stdout)
-		})
+	catch (err)
+	{
+		console.log(util.format(err))
 	}
-} catch (err) {
-	console.log(util.format(err))
-}
-}
 let file = require.resolve(__filename)
 fs.watchFile(file, () =>
 {
