a84f1dd07cba895378ddef89e5ef5552261e01c8
ManuelLove <104648635+ManuelLove@users.noreply.github.com>
2025-04-07 21:52:39 -0500

    Add files via upload

--- case.js
+++ case.js
@@ -685,9 +685,8 @@ if (m.mtype === 'interactiveResponseMessage' && m.message.interactiveResponseMes
 		const owner = JSON.parse(fs.readFileSync('./owner.json'))
 		const isShoNheOwn = owner.includes(senderNumber) || isBot
 		const isVip = prem.includes(senderNumber) || isShoNheOwn
-		const banned = JSON.parse(fs.readFileSync('./database/banned.json'))
-		const isBan = banned.includes(m.sender);
-if (isBan) return;
+		const banned = JSON.parse(fs.readFileSync('./database/banned.json'));
+const isBan = banned.includes(m.sender) || banned.includes("@" + m.sender);
 if (isBan && !isShoNheOwn) return;
 		const getQuoted = (m.quoted || m);
 		const quoted = (getQuoted.type == 'buttonsMessage') ? getQuoted[Object.keys(getQuoted)[1]] : (getQuoted.type == 'templateMessage') ? getQuoted.hydratedTemplate[Object.keys(getQuoted.hydratedTemplate)[1]] : (getQuoted.type == 'product') ? getQuoted[Object.keys(getQuoted)[0]] : m.quoted ? m.quoted : m
